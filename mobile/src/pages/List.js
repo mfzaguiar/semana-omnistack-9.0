e0a3461f656251b1a983a7c2c22f0ff3269f8b1d
mfzaguiar <first_mf@hotmail.com>
2019-10-05 21:40:39 -0300

    add websocket

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
+import socketio from "socket.io-client";
+
 import {
   SafeAreaView,
   ScrollView,
-  Text,
   Image,
   AsyncStorage,
-  StyleSheet
+  StyleSheet,
+  Alert
 } from "react-native";
 
 import SpotList from "../components/SpotList";
@@ -13,6 +15,22 @@ import logo from "../assets/logo.png";
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socket.io("http://192.168.0.109:3333", {
+        query: { user_id }
+      });
+    });
+
+    socket.on("booking_response", booking => {
+      Alert.alert(
+        `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+          booking.approved ? "APROVADA" : "REJEITADA"
+        }`
+      );
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem("techs").then(storedTechs => {
